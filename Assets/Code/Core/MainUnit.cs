ae16953253e257a643c4299b7a7142da31ebdfe9
Дон <dsvinkabot@gmail.com>
2022-03-08 01:47:23 +0300

    Обновление команд.

--- Assets/Code/Core/MainUnit.cs
+++ Assets/Code/Core/MainUnit.cs
@@ -1,25 +1,35 @@
 ﻿using Abstractions;
 using Abstractions.Commands;
+using Commands;
+using Core.CommandExecutors;
 using UnityEngine;
 
 namespace Core
 {
-    public sealed class MainUnit : MonoBehaviour, ISelectable, IAttackable
+    public sealed class MainUnit : MonoBehaviour, ISelectable, IAttackable, IDamageDealer
     {
+        private static readonly int Dead = Animator.StringToHash("Dead");
+        
         #region Serialize Fields
 
         [Header("Information")]
-        [SerializeField] private string _name;
+        [SerializeField] private string _name = "Unit";
         [SerializeField] private Sprite _icon;
         
         [Header("Settings")]
-        [SerializeField] private float _maxHealth;
+        [SerializeField] private int _damage = 25;
+        [SerializeField] private float _maxHealth = 100;
         [SerializeField] private Transform _pivotPoint;
-
+        
+        [Header("Компоненты")]
+        [SerializeField] private Animator _animator;
+        [SerializeField] private StopCommandExecutor _stopCommand;
+        
         #endregion
 
         #region Public Properties 
 
+        public int Damage => _damage;
         public float Health => _health;
         public float MaxHealth => _maxHealth;
         
@@ -38,6 +48,26 @@ namespace Core
         {
             _health = _maxHealth;
             _gameObject = gameObject;
+        }
+        
+        public void AddDamage(int amount)
+        {
+            if (_health <= 0)
+            {
+                return;
+            }
+            _health -= amount;
+            if (_health <= 0)
+            {
+                _animator.SetTrigger(Dead);
+                Invoke(nameof(Destroy), 1f);
+            }
+        }
+        
+        private async void Destroy()
+        {
+            await _stopCommand.ExecuteSpecific(new StopCommand());
+            Destroy(gameObject);
         }
     }
 }
---
+++ Assets/Code/UserControlSystem/Presenters/GameOverScreenPresenter.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Abstractions;
+using TMPro;
+using UniRx;
+using UnityEngine;
+using Zenject;
+
+namespace UserControlSystem.Presenters
+{
+    public sealed class GameOverScreenPresenter: MonoBehaviour
+    {
+        [Inject] private IGameStatus _gameStatus;
+
+        [SerializeField] private TMP_Text _text;
+        [SerializeField] private GameObject _view;
+
+        [Inject]
+        private void Init()
+        {
+            _gameStatus.Status.ObserveOnMainThread().Subscribe(result =>
+            {
+                if (result == 0)
+                {
+                    _text.text = "Ничья!";
+                }
+                else
+                {
+                    _text.text = $"Победила фракция №{result}";
+                }
+                _view.SetActive(true);
+                Time.timeScale = 0;
+            });
+        }
+
+    }
+}
